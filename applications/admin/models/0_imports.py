1fbbf1734fa79dee8f36e5ed9c42bb5aa604cf16
Massimo DiPierro <massimodipierro@Massimos-MacBook-Air.local>
2011-11-30 22:33:55 -0600

    fixed mercurial addremove (hg moved the funciton from cmdutil to scmutil)

--- applications/admin/models/0_imports.py
+++ applications/admin/models/0_imports.py
@@ -12,7 +12,7 @@ import socket
 from textwrap import dedent
 
 try:
-    from mercurial import ui, hg, cmdutil
+    from mercurial import ui, hg, cmdutil, scmutil
     have_mercurial = True
 except ImportError:
     have_mercurial = False
