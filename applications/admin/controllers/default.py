dc91ed5e61c0d0e697368689bb3b702f9f543412
Massimo Di Pierro <massimo.dipierro@gmail.com>
2011-12-25 12:35:34 -0600

    fixed twitter, thanks spametki

--- applications/admin/controllers/default.py
+++ applications/admin/controllers/default.py
@@ -1308,6 +1308,7 @@ def update_languages():
     session.flash = T('Language files (static strings) updated')
     redirect(URL('design',args=app,anchor='languages'))
 
+
 def twitter():
     session.forget()
     session._unlock(response)
@@ -1315,12 +1316,18 @@ def twitter():
     import gluon.contrib.simplejson as sj
     try:
         if TWITTER_HASH:
-            page = gluon.tools.fetch('http://twitter.com/%s?format=json'%TWITTER_HASH)
-            return sj.loads(page)['#timeline']
+            page = urllib.urlopen("http://search.twitter.com/search.json?q=%%40%s" % TWITTER_HASH).read()
+            data = sj.loads(page  , encoding="utf-8")['results']
+            d = dict()
+            for e in data:
+                d[e["id"]] = e
+            r = reversed(sorted(d))
+            return dict(tweets = [d[k] for k in r])
         else:
             return 'disabled'
     except Exception, e:
         return DIV(T('Unable to download because:'),BR(),str(e))
+
 
 def user():
     if MULTI_USER_MODE:
