b829109e330f6e83cd89d08445db6e401d446879
Massimo Di Pierro <mdipierro@OfficeMacPro.local>
2012-02-02 16:35:40 -0600

    socket timeout = 5 seconds until better solution

--- gluon/widget.py
+++ gluon/widget.py
@@ -555,10 +555,10 @@ def console():
                       help='timeout on shutdown of server (5 seconds)')
 
     parser.add_option('--socket-timeout',
-                      default=60,
+                      default=5,
                       type='int',
                       dest='socket_timeout',
-                      help='timeout for socket (60 second)')
+                      help='timeout for socket (5 second)')
 
     parser.add_option('-f',
                       '--folder',
