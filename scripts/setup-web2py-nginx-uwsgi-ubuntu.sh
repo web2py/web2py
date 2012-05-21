0ae599a94d7452ed46fd0dd35c42a865e569d645
Massimo DiPierro <massimodipierro@me.(none)>
2012-05-21 16:50:03 -0500

    better setup-web2py-nginx-uwsgi-ubuntu.sh, issue 816, thanks Orsomannaro

--- scripts/setup-web2py-nginx-uwsgi-ubuntu.sh
+++ scripts/setup-web2py-nginx-uwsgi-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'setup-web2py-nginx-uwsgi-ubuntu.sh'
-echo 'Requires Ubuntu 10.04 (LTS) and installs Nginx + uWSGI + Web2py'
+echo 'setup-web2py-nginx-uwsgi-ubuntu-precise.sh'
+echo 'Requires Ubuntu 12.04 and installs Nginx + uWSGI + Web2py'
 
 # Get Web2py Admin Password
 echo -e "Web2py Admin Password: \c "
@@ -10,12 +10,11 @@ read  PW
 # Upgrade and install needed software
 apt-get update
 apt-get -y upgrade
-apt-get install python-software-properties
-add-apt-repository ppa:nginx/stable
-add-apt-repository  ppa:uwsgi/release
-apt-get update
+apt-get -y dist-upgrade
+apt-get autoremove
+apt-get autoclean
 apt-get -y install nginx-full
-apt-get -y install uwsgi-python
+apt-get -y install uwsgi uwsgi-plugin-python
 
 # Create configuration file /etc/nginx/sites-available/web2py
 echo 'server {
@@ -49,33 +48,32 @@ server {
 
 ln -s /etc/nginx/sites-available/web2py /etc/nginx/sites-enabled/web2py
 rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
 mkdir /etc/nginx/ssl
 cd /etc/nginx/ssl
 openssl genrsa -out web2py.key 1024
 openssl req -batch -new -key web2py.key -out web2py.csr
 openssl x509 -req -days 1780 -in web2py.csr -signkey web2py.key -out web2py.crt
 
-# Create configuration file /etc/uwsgi-python/apps-available/web2py.xml
+# Create configuration file /etc/uwsgi/apps-available/web2py.xml
 echo '<uwsgi>
+    <plugin>python</plugin>
     <socket>127.0.0.1:9001</socket>
     <pythonpath>/home/www-data/web2py/</pythonpath>
     <app mountpoint="/">
         <script>wsgihandler</script>
     </app>
-</uwsgi>' >/etc/uwsgi-python/apps-available/web2py.xml
-ln -s /etc/uwsgi-python/apps-available/web2py.xml /etc/uwsgi-python/apps-enabled/web2py.xml
+</uwsgi>' >/etc/uwsgi/apps-available/web2py.xml
+ln -s /etc/uwsgi/apps-available/web2py.xml /etc/uwsgi/apps-enabled/web2py.xml
 
 # Install Web2py
 apt-get -y install unzip
-cd /home
-mkdir www-data
-cd www-data
+mkdir /home/www-data
+cd /home/www-data
 wget http://web2py.com/examples/static/web2py_src.zip
 unzip web2py_src.zip
 rm web2py_src.zip
 chown -R www-data:www-data web2py
 cd /home/www-data/web2py
 sudo -u www-data python -c "from gluon.main import save_password; save_password('$PW',443)"
-/etc/init.d/uwsgi-python restart
+/etc/init.d/uwsgi restart
 /etc/init.d/nginx restart
