dbe83a2b5eb917280161993693458e5a41baf756
mdipierro <massimo.dipierro@gmail.com>
2012-07-18 13:40:10 -0500

    added some more missing files

---
+++ applications/welcome/languages/plural-en.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+{
+# "singular form (0)": ["first plural form (1)", "second plural form (2)", ...],
+'account': ['accounts'],
+'book': ['books'],
+'is': ['are'],
+'man': ['men'],
+'person': ['people'],
+'shop': ['shops'],
+'this': ['these'],
+'was': ['were'],
+'woman': ['women'],
+}
