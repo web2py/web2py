e5d10fe7f7310a78bf41728d93dbb28378634366
Andrew Replogle <areplogle@ARThinkPad.(none)>
2012-07-19 15:51:58 -0500

    removed some cruft

--- applications/admin/controllers/git.py
+++
@@ -1,7 +0,0 @@
-# coding: utf8
-# try something like
-def index(): return dict(message="hello from git.py")
-
-def import_url(git_url): 
-    foo = 'bar'
-    return dict(message="imported whatever")
