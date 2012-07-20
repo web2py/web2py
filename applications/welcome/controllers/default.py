cfd74ff935d83182a31ec6d052b137d23b124e15
Andrew Replogle <areplogle@ARThinkPad.(none)>
2012-07-20 09:17:39 -0500

    removed some runtime files from developing

---
+++ applications/welcome/controllers/default.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a samples controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+    """
+    response.flash = T("Welcome to web2py!")
+    return dict(message=T('Hello World'))
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request,db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
+@auth.requires_signature()
+def data():
+    """
+    http://..../[app]/default/data/tables
+    http://..../[app]/default/data/create/[table]
+    http://..../[app]/default/data/read/[table]/[id]
+    http://..../[app]/default/data/update/[table]/[id]
+    http://..../[app]/default/data/delete/[table]/[id]
+    http://..../[app]/default/data/select/[table]
+    http://..../[app]/default/data/search/[table]
+    but URLs must be signed, i.e. linked with
+      A('table',_href=URL('data/tables',user_signature=True))
+    or with the signed load operator
+      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
+    """
+    return dict(form=crud())
---
+++ applications/welcome/static/js/web2py.js
@@ -0,0 +1,161 @@
+function popup(url) {
+  newwindow=window.open(url,'name','height=400,width=600');
+  if (window.focus) newwindow.focus();
+  return false;
+}
+function collapse(id) { jQuery('#'+id).slideToggle(); }
+function fade(id,value) { if(value>0) jQuery('#'+id).hide().fadeIn('slow'); else jQuery('#'+id).show().fadeOut('slow'); }
+function ajax(u,s,t) {
+    query = '';
+    if (typeof s == "string") {
+        d = jQuery(s).serialize();
+        if(d){ query = d; }
+    } else {
+        pcs = [];
+        if (s != null && s != undefined) for(i=0; i<s.length; i++) {
+            q = jQuery("[name="+s[i]+"]").serialize();
+            if(q){pcs.push(q);}
+        }
+        if (pcs.length>0){query = pcs.join("&");}
+    }
+    jQuery.ajax({type: "POST", url: u, data: query, success: function(msg) { if(t) { if(t==':eval') eval(msg); else if(typeof t=='string') jQuery("#"+t).html(msg); else t(msg); } } });
+}
+
+String.prototype.reverse = function () { return this.split('').reverse().join('');};
+function web2py_ajax_fields(target) {
+  var date_format = (typeof w2p_ajax_date_format != 'undefined') ? w2p_ajax_date_format : "%Y-%m-%d";
+  var datetime_format = (typeof w2p_ajax_datetime_format != 'undefined') ? w2p_ajax_datetime_format : "%Y-%m-%d %H:%M:%S";
+  jQuery("input.date",target).each(function() {Calendar.setup({inputField:this, ifFormat:date_format, showsTime:false });});
+  jQuery("input.datetime",target).each(function() {Calendar.setup({inputField:this, ifFormat:datetime_format, showsTime: true, timeFormat: "24" });});
+  jQuery("input.time",target).each(function(){jQuery(this).timeEntry();});
+
+};
+
+function web2py_ajax_init(target) {
+  jQuery('.hidden', target).hide();
+  jQuery('.error', target).hide().slideDown('slow');
+  web2py_ajax_fields(target);
+};
+
+function web2py_event_handlers() {
+  var doc = jQuery(document)
+  doc.on('click', '.flash', function(e){jQuery(this).fadeOut('slow'); e.preventDefault();});
+  doc.on('keyup', 'input.integer', function(){this.value=this.value.reverse().replace(/[^0-9\-]|\-(?=.)/g,'').reverse();});
+  doc.on('keyup', 'input.double, input.decimal', function(){this.value=this.value.reverse().replace(/[^0-9\-\.,]|[\-](?=.)|[\.,](?=[0-9]*[\.,])/g,'').reverse();});
+  var confirm_message = (typeof w2p_ajax_confirm_message != 'undefined') ? w2p_ajax_confirm_message : "Are you sure you want to delete this object?";
+  doc.on('click', "input[type='checkbox'].delete", function(){if(this.checked) if(!confirm(confirm_message)) this.checked=false;});
+};
+
+jQuery(function() {
+   var flash = jQuery('.flash');
+   flash.hide();
+   if(flash.html()) flash.slideDown();
+   web2py_ajax_init(document);
+   web2py_event_handlers();
+});
+
+function web2py_trap_form(action,target) {
+   jQuery('#'+target+' form').each(function(i){
+      var form=jQuery(this);
+      if(!form.hasClass('no_trap'))
+        form.submit(function(e){
+         jQuery('.flash').hide().html('');
+         web2py_ajax_page('post',action,form.serialize(),target);
+	 e.preventDefault();
+      });
+   });
+}
+
+function web2py_trap_link(target) {
+    jQuery('#'+target+' a.w2p_trap').each(function(i){
+	    var link=jQuery(this);
+	    link.click(function(e) {
+		    jQuery('.flash').hide().html('');
+		    web2py_ajax_page('get',link.attr('href'),[],target);
+		    e.preventDefault();
+		});
+	});
+}
+
+function web2py_ajax_page(method, action, data, target) {
+  jQuery.ajax({'type':method, 'url':action, 'data':data,
+    'beforeSend':function(xhr) {
+      xhr.setRequestHeader('web2py-component-location', document.location);
+      xhr.setRequestHeader('web2py-component-element', target);},
+    'complete':function(xhr,text){
+      var html=xhr.responseText;
+      var content=xhr.getResponseHeader('web2py-component-content');
+      var command=xhr.getResponseHeader('web2py-component-command');
+      var flash=xhr.getResponseHeader('web2py-component-flash');
+      var t = jQuery('#'+target);
+      if(content=='prepend') t.prepend(html);
+      else if(content=='append') t.append(html);
+      else if(content!='hide') t.html(html);
+      web2py_trap_form(action,target);
+      web2py_trap_link(target);
+      web2py_ajax_init('#'+target);
+      if(command)
+	  eval(decodeURIComponent(command));
+      if(flash)
+	  jQuery('.flash').html(decodeURIComponent(flash)).slideDown();
+      }
+    });
+}
+
+function web2py_component(action, target, timeout, times){
+  jQuery(function(){
+    var element = jQuery("#" + target).get(0);
+    var statement = "jQuery('#" + target + "').get(0).reload();";
+    element.reload = function (){
+        // Continue if times is Infinity or
+        // the times limit is not reached
+        if (this.reload_check()){
+            web2py_ajax_page('get', action, null, target);} }; // reload
+    // Method to check timing limit
+    element.reload_check = function (){
+        if (this.reload_counter == Infinity){return true;}
+        else {
+            if (!isNaN(this.reload_counter)){
+                this.reload_counter -= 1;
+                if (this.reload_counter < 0){
+                    if (!this.run_once){
+                        clearInterval(this.timing);
+                        return false;
+                    }
+                }
+                else{return true;}
+            } }
+            return false;}; // reload check
+    if (!isNaN(timeout)){
+        element.timeout = timeout;
+        element.reload_counter = times;
+        if (times > 1){
+        // Multiple or infinite reload
+        // Run first iteration
+        web2py_ajax_page('get', action, null, target);
+        element.run_once = false;
+        element.timing = setInterval(statement, timeout);
+        element.reload_counter -= 1;
+        }
+        else if (times == 1) {
+        // Run once with timeout
+        element.run_once = true;
+        element.setTimeout = setTimeout;
+        element.timing = setTimeout(statement, timeout);
+        }
+    } else {
+        // run once (no timeout specified)
+	element.reload_counter = Infinity;
+        web2py_ajax_page('get', action, null, target);
+    } }); }
+
+function web2py_comet(url,onmessage,onopen,onclose) {
+  if ("WebSocket" in window) {
+    var ws = new WebSocket(url);
+    ws.onopen = onopen?onopen:(function(){});
+    ws.onmessage = onmessage;
+    ws.onclose = onclose?onclose:(function(){});
+    return true; // supported
+  } else return false; // not supported
+}
+
