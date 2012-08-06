26db9fff271d3f50e690455b08361d32001da3f6
mdipierro <massimo.dipierro@gmail.com>
2012-08-06 07:25:24 -0500

    fixed copyright year, where possible

--- applications/welcome/models/menu.py
+++ applications/welcome/models/menu.py
@@ -13,7 +13,7 @@ response.meta.author = 'Your Name <you@example.com>'
 response.meta.description = 'a cool new app'
 response.meta.keywords = 'web2py, python, framework'
 response.meta.generator = 'Web2py Web Framework'
-response.meta.copyright = 'Copyright 2011'
+response.meta.copyright = 'Copyright 2012'
 
 ## your http://google.com/analytics id
 response.google_analytics_id = None
