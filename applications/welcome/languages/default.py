ecba8d44a9750f7bfd06055403f68de4cb698376
mdipierro <massimo.dipierro@gmail.com>
2012-08-27 13:10:52 -0500

    better test_languages.py

--- applications/welcome/languages/default.py
+++ applications/welcome/languages/default.py
@@ -2,4 +2,12 @@
 {
 '!langcode!': 'en-us',
 '!langname!': 'English (US)',
+'%s %%(shop)': '%s %%(shop)',
+'%s %%(shop[0])': '%s %%(shop[0])',
+'%s %%{shop[0]}': '%s %%{shop[0]}',
+'%s %%{shop}': '%s %%{shop}',
+'@markmin\x01**Hello World**': '**Hello World**',
+'Hello World': 'Hello World',
+'Hello World  ## comment': 'Hello World  ',
+'Hello World## comment': 'Hello World',
 }
