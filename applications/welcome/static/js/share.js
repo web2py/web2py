dbf6ce6d6c64de889470b8d101a3acffdca46480
Nico Zanferrari <nicozanf@gmail.com>
2012-09-22 00:06:21 +0200

    Modified share to Buzz to Google+

--- applications/welcome/static/js/share.js
+++ applications/welcome/static/js/share.js
@@ -21,8 +21,8 @@ jQuery(function(){
 	var title = escape(jQuery('title').text());
 	var twit = 'http://twitter.com/home?status='+title+'%20'+url;
 	var facebook = 'http://www.facebook.com/sharer.php?u='+url;
-	var buzz = 'https://plus.google.com/share?url='+url;
-	var tbar = '<div id="socialdrawer"><span>Share<br/></span><div id="sicons"><a href="'+twit+'" id="twit" title="Share on twitter"><img src="'+path+'/twitter.png"  alt="Share on Twitter" width="32" height="32" /></a><a href="'+facebook+'" id="facebook" title="Share on Facebook"><img src="'+path+'/facebook.png"  alt="Share on facebook" width="32" height="32" /></a><a href="'+buzz+'" id="buzz" title="Share on Buzz"><img src="'+path+'/google-buzz.png"  alt="Share on Buzz" width="32" height="32" /></a></div></div>';	
+	var gplus = 'https://plus.google.com/share?url='+url;
+	var tbar = '<div id="socialdrawer"><span>Share<br/></span><div id="sicons"><a href="'+twit+'" id="twit" title="Share on twitter"><img src="'+path+'/twitter.png"  alt="Share on Twitter" width="32" height="32" /></a><a href="'+facebook+'" id="facebook" title="Share on Facebook"><img src="'+path+'/facebook.png"  alt="Share on facebook" width="32" height="32" /></a><a href="'+gplus+'" id="gplus" title="Share on Google Plus"><img src="'+path+'/gplus-32.png"  alt="Share on Google Plus" width="32" height="32" /></a></div></div>';	
 	// Add the share tool bar.
 	jQuery('body').append(tbar); 
 	var st = jQuery('#socialdrawer');
