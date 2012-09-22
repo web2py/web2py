f4cc8137405289ddae166f70dfac66fb0454286c
Michele Comitini <mcm@glisco.it>
2012-09-22 23:37:06 +0200

    fixed apos encoding.

--- gluon/html.py
+++ gluon/html.py
@@ -34,7 +34,7 @@ join = ''.join
 
 # name2codepoint is incomplete respect to xhtml (and xml): 'apos' is missing.
 entitydefs = dict(map(lambda (k,v): (k, unichr(v).encode('utf-8')), name2codepoint.iteritems()))
-entitydefs.setdefault('apos', u"'")
+entitydefs.setdefault('apos', u"'".encode('utf-8'))
 
 
 __all__ = [
