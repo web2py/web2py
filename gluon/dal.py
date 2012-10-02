6cc29e452731375cab550db830a5f73bc331edfb
mdipierro <massimo.dipierro@gmail.com>
2012-10-01 19:15:37 -0500

    fixed DAL.__new__ and SQLFORM.factory

--- gluon/dal.py
+++ gluon/dal.py
@@ -6572,7 +6572,10 @@ class DAL(object):
                                     Field('fieldname2'))
     """
 
-    def __new__(cls, uri='sqlite://dummy.db', *args, **kwargs):
+    def __new__(cls, uri='sqlite://dummy.db', *args, **kwargs):        
+        if uri==None and not 'singleton_code' in kwargs:
+            # this deal with the special case of Dummy DAL for SQLFORM.factory
+            return super(DAL, cls).__new__(cls, uri, *args, **kwargs)
         if not hasattr(THREAD_LOCAL,'db_instances'):
             THREAD_LOCAL.db_instances = {}
         if 'singleton_code' in kwargs:
