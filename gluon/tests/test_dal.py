5f67edbf87b7df53341b0eda6f9ac8fa933a32e6
mdipierro <massimo.dipierro@gmail.com>
2012-10-23 11:30:02 -0500

    dal almost runs with python 3.3 but fails a test on 2.5

--- gluon/tests/test_dal.py
+++ gluon/tests/test_dal.py
@@ -13,7 +13,10 @@ else:
 
 import unittest
 import datetime
-import cStringIO
+try:
+    import cStringIO as StringIO
+except:
+    from io import StringIO
 from dal import DAL, Field, Table, SQLALL
 
 ALLOWED_DATATYPES = [
@@ -555,11 +558,11 @@ class TestImportExportFields(unittest.TestCase):
                 id = db.person.insert(name=str(k))
                 db.pet.insert(friend=id,name=str(k))
         db.commit()
-        stream = cStringIO.StringIO()
+        stream = StringIO.StringIO()
         db.export_to_csv_file(stream)
         db(db.pet).delete()
         db(db.person).delete()
-        stream = cStringIO.StringIO(stream.getvalue())
+        stream = StringIO.StringIO(stream.getvalue())
         db.import_from_csv_file(stream)
         assert db(db.person.id==db.pet.friend)(db.person.name==db.pet.name).count()==10
         db.pet.drop()
@@ -579,9 +582,9 @@ class TestImportExportUuidFields(unittest.TestCase):
                 id = db.person.insert(name=str(k),uuid=str(k))
                 db.pet.insert(friend=id,name=str(k))
         db.commit()
-        stream = cStringIO.StringIO()
+        stream = StringIO.StringIO()
         db.export_to_csv_file(stream)
-        stream = cStringIO.StringIO(stream.getvalue())
+        stream = StringIO.StringIO(stream.getvalue())
         db.import_from_csv_file(stream)
         assert db(db.person).count()==10
         assert db(db.person.id==db.pet.friend)(db.person.name==db.pet.name).count()==20
