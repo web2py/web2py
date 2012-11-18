d7a38b5350eda3b2aac870de22db55bd3c48caea
mdipierro <massimo.dipierro@gmail.com>
2012-11-17 22:14:42 -0600

    better heroku support

---
+++ scripts/setup-web2py-heroku.sh
@@ -0,0 +1,15 @@
+read -p "Choose your admin password?" passwd
+sudo pip install virtualenv
+sudo pip install postgresql2
+virtualenv venv --distribute
+source venv/bin/activate
+pip freeze > requirements.txt
+echo "web: python web2py.py -a '"$passwd"' -i 0.0.0.0 -p $PORT" > Procfile
+git init
+git add .
+git add Procfile
+git commit -a -m "first commit"
+heroku create
+git push heroku master
+heroku scale web=1
+heroku open
