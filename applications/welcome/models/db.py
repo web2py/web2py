f9eb2c5a0a0ac9a20e26ee3959ba14bc3aa47f5a
mdipierro <massimo.dipierro@gmail.com>
2012-12-03 16:30:13 -0600

    check_reserved in welcome, thanks Marin

--- applications/welcome/models/db.py
+++ applications/welcome/models/db.py
@@ -11,7 +11,7 @@
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL('sqlite://storage.sqlite')
+    db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
     db = DAL('google:datastore')
