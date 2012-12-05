8e48a816f80ed677e299a45aeb77ce2c3ed7a473
mdipierro <massimo.dipierro@gmail.com>
2012-12-05 15:31:38 -0600

    better paymenttech support, BSD license

--- gluon/contrib/paymentech.py
+++ gluon/contrib/paymentech.py
@@ -3,23 +3,11 @@
 
 # This module provides a simple API for Paymentech(c) payments
 # The original code was taken from this web2py issue post
-# http://code.google.com/p/web2py/issues/detail?id=1170
+# http://code.google.com/p/web2py/issues/detail?id=1170 by Adnan Smajlovic
 #
 #    Copyright (C) <2012>  Alan Etkin <spametki@gmail.com>
-#                          Adnan Smajlovic
+#    License: BSD
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys, httplib, urllib, urllib2
 from xml.dom.minidom import parseString
