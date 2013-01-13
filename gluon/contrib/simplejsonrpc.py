e363ee0994b4464530a36b724f962b78be9b5e0c
TierraDelFuego <you@example.com>
2013-01-12 23:34:35 -0800

    Change the call to python from /usr/bin/python to /usr/bin/env python so the correct python is called when using virtualenv. Remove the the call if not needed.

--- gluon/contrib/simplejsonrpc.py
+++ gluon/contrib/simplejsonrpc.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by the
