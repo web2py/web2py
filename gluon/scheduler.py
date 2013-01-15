ad684bfdfcf10bf80bc81f92795031a24dc0f228
Massimo <massimo@mdipierro.local>
2013-01-15 15:54:29 -0600

    scheduler patch, thanks Niphlod

--- gluon/scheduler.py
+++ gluon/scheduler.py
@@ -556,7 +556,7 @@ class Scheduler(MetaScheduler):
             self.die()
 
     def wrapped_assign_tasks(self, db):
-        db.commit()  # ?don't know if it's useful, let's be completely sure
+        db.commit()  #db.commit() only for Mysql
         x = 0
         while x < 10:
             try:
@@ -571,6 +571,7 @@ class Scheduler(MetaScheduler):
 
     def wrapped_pop_task(self):
         db = self.db
+        db.commit() #another nifty db.commit() only for Mysql
         x = 0
         while x < 10:
             try:
