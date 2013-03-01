0193db35b67f11274034a068f86b662c4ecd7c0c
Massimo <massimo@mdipierro.local>
2013-03-01 12:46:20 -0600

    added Czech, thanks you David Herman

---
+++ applications/welcome/languages/plural-cs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+{
+# "singular form (0)": ["first plural form (1)", "second plural form (2)", ...],
+'vte?ina': ['vte?iny', 'vte?in'],
+'vte?inou': ['vte?inami', 'vte?inami'],
+'minuta': ['minuty', 'minut'],
+'minutou': ['minutami', 'minutami'],
+'hodina': ['hodiny','hodin'],
+'hodinou': ['hodinami','hodinami'],
+'den': ['dny','dn?'],
+'dnem': ['dny','dny'],
+'t?den': ['t?dny','t?dn?'],
+'t?dnem': ['t?dny','t?dny'],
+'m?s?c': ['m?s?ce','m?s?c?'],
+'m?s?cem': ['m?s?ci','m?s?ci'],
+'rok': ['roky','let'],
+'rokem': ['roky','lety'],
+'z?znam': ['z?znamy', 'z?znam?'],
+'soubor': ['soubory', 'soubor?']
+}
