887d4cc13651fa4c5383975725e236ad5ebf433e
mdipierro <massimo.dipierro@gmail.com>
2013-04-15 18:05:59 -0500

    enabled doctests (python 2.7 only)

--- gluon/tests/__init__.py
+++ gluon/tests/__init__.py
@@ -11,3 +11,7 @@ from test_utils import *
 from test_contribs import *
 from test_markmin import *
 from test_web import *
+
+import sys
+if sys.version[:3] == '2.7':
+    from test_old_doctests import *
