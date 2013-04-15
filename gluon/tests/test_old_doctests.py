7341988b01ae9ace89afe42a8322536d3c369bba
mdipierro <massimo.dipierro@gmail.com>
2013-04-15 17:19:11 -0500

    hooking old doctests into unittests, thanks Niphlod

---
+++ gluon/tests/test_old_doctests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Unit tests for old doctests in validators.py, utf8.py, html.py.
+    Don't abuse doctests, web2py > 2.4.5 will accept only unittests
+"""
+import sys
+import os
+if os.path.isdir('gluon'):
+    sys.path.append(os.path.realpath('gluon'))
+else:
+    sys.path.append(os.path.realpath('../'))
+
+import unittest
+import doctest
+               
+def load_tests(loader, tests, ignore):
+
+    tests.addTests(
+        doctest.DocTestSuite('validators',
+            optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+        )
+    )
+    tests.addTests(
+        doctest.DocTestSuite('html')
+        )
+    tests.addTests(
+        doctest.DocTestSuite('utf8')
+        )
+    return tests
+
+if __name__ == '__main__':
+    unittest.main()
