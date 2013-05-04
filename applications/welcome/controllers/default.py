d431206e30e11dc70c44ebd41b543c02864922db
Tim Richardson <tim@tim-richardson.net>
2013-05-04 22:13:15 +1000

    Fixed a very minor typo

--- applications/welcome/controllers/default.py
+++ applications/welcome/controllers/default.py
@@ -2,7 +2,7 @@
 # this file is released under public domain and you can use without limitations
 
 #########################################################################
-## This is a samples controller
+## This is a sample controller
 ## - index is the default action of any application
 ## - user is required for authentication and authorization
 ## - download is for downloading files uploaded in the db (does streaming)
