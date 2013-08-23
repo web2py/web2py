f17270291e7bb150373dcc404c8054cac690b930
mdipierro <massimo.dipierro@gmail.com>
2013-08-23 14:22:21 -0500

    removed customize it

--- applications/welcome/models/menu.py
+++ applications/welcome/models/menu.py
@@ -8,7 +8,7 @@
 response.logo = A(B('web',SPAN(2),'py'),XML('&trade;&nbsp;'),
                   _class="brand",_href="http://www.web2py.com/")
 response.title = request.application.replace('_',' ').title()
-response.subtitle = T('customize me!')
+response.subtitle = ''
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
 response.meta.author = 'Your Name <you@example.com>'
