f5f6f365f9908c1c98cc6a5b7086568befa23be9
niphlod <niphlod@gmail.com>
2013-08-25 18:35:29 +0200

    added boilerplate to be able to run tests from anywhere. Added test for validate_and_insert too.

--- gluon/tests/test_languages.py
+++ gluon/tests/test_languages.py
@@ -7,16 +7,41 @@
 
 import sys
 import os
-if os.path.isdir('gluon'):
-    sys.path.append(os.path.realpath('gluon'))
-else:
-    sys.path.append(os.path.realpath('../'))
-
 import unittest
-import languages
 import tempfile
 import threading
 import logging
+
+
+def fix_sys_path():
+    """
+    logic to have always the correct sys.path
+     '', web2py/gluon, web2py/site-packages, web2py/ ...
+    """
+
+    def add_path_first(path):
+        sys.path = [path] + [p for p in sys.path if (
+            not p == path and not p == (path + '/'))]
+
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    if not os.path.isfile(os.path.join(path,'web2py.py')):
+        i = 0
+        while i<10:
+            i += 1
+            if os.path.exists(os.path.join(path,'web2py.py')):
+                break
+            path = os.path.abspath(os.path.join(path, '..'))
+
+    paths = [path,
+             os.path.abspath(os.path.join(path, 'site-packages')),
+             os.path.abspath(os.path.join(path, 'gluon')),
+             '']
+    [add_path_first(path) for path in paths]
+fix_sys_path()
+
+
+import languages
 from storage import Storage
 
 try:
