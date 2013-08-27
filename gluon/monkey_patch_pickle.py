c997192277337d2c04497e4248d95f858636333f
mdipierro <massimo.dipierro@gmail.com>
2013-08-27 16:12:23 -0500

    another possible solution to issue 1524, without monkey patching

--- gluon/monkey_patch_pickle.py
+++
@@ -1,9 +0,0 @@
-from pickle import Pickler, MARK, DICT, EMPTY_DICT
-from types import DictionaryType
-
-def save_dict(self, obj):
-     self.write(EMPTY_DICT if self.bin else MARK+DICT)
-     self.memoize(obj)
-     self._batch_setitems([(key,obj[key]) for key in sorted(obj)])
-
-Pickler.dispatch[DictionaryType] = save_dict          
