a626c213a77ec0a2992d647b8a077b151434b4ab
mdipierro <massimo.dipierro@gmail.com>
2013-08-27 17:18:27 -0500

    exists to isdir, thanks Niphlod

--- handlers/modpythonhandler.py
+++ handlers/modpythonhandler.py
@@ -35,7 +35,7 @@ from mod_python import apache
 path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path)
 
-if not os.path.exists('applications'):
+if not os.path.isdir('applications'):
     raise RuntimeError('Running from the wrong folder')
 
 sys.path = [path] + [p for p in sys.path if not p == path]
