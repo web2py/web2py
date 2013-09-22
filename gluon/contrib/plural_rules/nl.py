d547a9d6ea67847ffc19af3ee7c00ea6ca809b84
mdipierro <massimo.dipierro@gmail.com>
2013-09-22 10:10:28 -0500

    added nl, es plural rules, thanks Vladyslav

--- gluon/contrib/plural_rules/nl.py
+++ gluon/contrib/plural_rules/nl.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-# Plural-Forms for es (Spanish)
+# Plural-Forms for nl (Dutch (Netherlands))
 
-nplurals=2  # Spanish language has 2 forms:
+nplurals=2  # Dutch has 2 forms:
             # 1 singular and 1 plural
 
 # Determine plural_id for number *n* as sequence of positive
@@ -13,8 +13,5 @@ get_plural_id = lambda n: int(n != 1)
 # Construct and return plural form of *word* using
 # *plural_id* (which ALWAYS>0). This function will be executed
 # for words (or phrases) not found in plural_dict dictionary
-construct_plural_form = lambda word, plural_id: (word +
-                         ('es' if word[-1:] in ('s', 'x', 'y', 'l', 'r', 'n', 'd', 'z', 'j') or
-                                  word[-2:] == 'ch'
-                          else 's'))
+# construct_plural_form = lambda word, plural_id: (word + 'suffix')
 
