b67141b1ef2e7240b172e633d67105d70b03e2ec
Massimo <massimo@mdipierro.local>
2013-09-24 13:29:54 -0500

    fixed problem with ENABLED/DISABLED, issue 1689

--- gluon/main.py
+++ gluon/main.py
@@ -396,15 +396,8 @@ def wsgibase(environ, responder):
                         raise HTTP(404, rwthread.routes.error_message
                                    % 'invalid request',
                                    web2py_error='invalid application')
-                elif request.is_local and exists(disabled):
-                    data = dict([item.strip() for item in line.split(':',1)]
-                                for line in open(disabled) if line.strip())
-                    if data.get('disabled','True').lower() != 'false':
-                        if 'redirect' in data:
-                            redirect(data['redirect'])
-                        if 'message' in data:
-                            raise HTTP(503, data['message'])
-                        raise HTTP(503, "<html><body><h1>Temporarily down for maintenance</h1></body></html>")
+                elif not request.is_local and exists(disabled):
+                    raise HTTP(503, "<html><body><h1>Temporarily down for maintenance</h1></body></html>")
 
                 # ##################################################
                 # build missing folders
