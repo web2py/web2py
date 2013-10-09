e563ae9111b741968b59607584a718c6ef1f87c2
Tim Richardson <tim@tim-richardson.net>
2013-10-10 10:20:29 +1100

    fix virtual fields with SQLFORM.grid fields=

--- gluon/sqlhtml.py
+++ gluon/sqlhtml.py
@@ -1912,11 +1912,18 @@ class SQLFORM(FORM):
             for join in left:
                 tablenames += db._adapter.tables(join)
         tables = [db[tablename] for tablename in tablenames]
+       
         if fields:
+             #add missing tablename to virtual fields
+            for table in tables:
+                for k,f in table.iteritems():
+                    if isinstance(f,Field.Virtual):
+                        f.tablename = table._tablename
             columns = [f for f in fields if f.tablename in tablenames]
         else:
             fields = []
             columns = []
+            virtual_columns = []
             for table in tables:
                 for k,f in table.iteritems():
                     if not k.startswith('_'):
@@ -1926,8 +1933,10 @@ class SQLFORM(FORM):
                                 columns.append(f) # these are displayed
                         elif isinstance(f,Field.Virtual) and f.readable:
                             f.tablename = table._tablename
-                            columns.append(f)
-                            fields.append(f)
+                            #show virtual fields after real fields so put at end of list
+                            virtual_columns.append(f) #add to fields as well
+            fields = fields + virtual_columns
+            columns = columns + virtual_columns
                         
         if not field_id:
             if groupby is None:
@@ -2271,11 +2280,9 @@ class SQLFORM(FORM):
             limitby = (paginate*page,paginate*(page+1))
         else:
             limitby = None
-
         try:
-            table_fields = filter(
-                lambda f: (f.tablename in tablenames) and \
-                    (not(isinstance(f,Field.Virtual))),fields)
+            table_fields = [field for field in fields
+                            if (field.tablename in tablenames and not(isinstance(field,Field.Virtual)))]
             if dbset._db._adapter.dbengine=='google:datastore':
                 rows = dbset.select(left=left,orderby=orderby,
                                     groupby=groupby,limitby=limitby,
