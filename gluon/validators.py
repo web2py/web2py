e56e37689499ceb115676e08bf57a4bd153b3676
mdipierro <massimo.dipierro@gmail.com>
2014-01-03 20:58:57 -0600

    fixed issue 1836, Problem with IS_IN_DB(label='%(x)3.3s')

--- gluon/validators.py
+++ gluon/validators.py
@@ -458,7 +458,7 @@ class IS_IN_SET(Validator):
 
 
 regex1 = re.compile('\w+\.\w+')
-regex2 = re.compile('%\((?P<name>[^\)]+)\)s')
+regex2 = re.compile('%\((?P<name>[^\)]+)\)(\d*\.\d*)?[a-zA-Z]')
 
 
 class IS_IN_DB(Validator):
