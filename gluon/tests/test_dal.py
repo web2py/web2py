ebbce2cdbc56068ac123e2e9d043f9d9c3909b0b
Alan Etkin <spametki@gmail.com>
2014-01-20 16:17:17 -0300

    Change sdk path in test_dal_nosql (webclient driver issue)

--- gluon/tests/test_dal.py
+++ gluon/tests/test_dal.py
@@ -50,6 +50,9 @@ print 'Testing against %s engine (%s)' % (DEFAULT_URI.partition(':')[0], DEFAULT
 
 # setup non-sql unit testing environment
 if "google" in DEFAULT_URI:
+    sdk = os.path.join(os.getcwd(), "google_appengine")
+    print "path to sdk is", sdk
+    sys.path.append(sdk)
     from google.appengine.ext import testbed
     gaetestbed = testbed.Testbed()
     gaetestbed.activate()
