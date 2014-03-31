fab4d1e7828b1daa8cc8c6fdca2803aa819cab2c
mdipierro <massimo.dipierro@gmail.com>
2014-03-31 00:24:52 -0500

    welcome + gae should use ndb as example

--- applications/welcome/models/db.py
+++ applications/welcome/models/db.py
@@ -14,7 +14,7 @@ if not request.env.web2py_runtime_gae:
     db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore')
+    db = DAL('google:datastore+ndb')
     ## store sessions and tickets there
     session.connect(request, response, db=db)
     ## or store session in Memcache, Redis, etc.
