5c9282aca50657058cb9ee4f62c6d7e9432fe66b
mdipierro <massimo.dipierro@gmail.com>
2014-08-08 00:50:42 -0500

    fixed issue 1947, raw args in expose, thanks iiijjjiii

--- gluon/tools.py
+++ gluon/tools.py
@@ -5379,8 +5379,11 @@ class Expose(object):
         base = base or os.path.join(current.request.folder, 'static')
         basename = basename or current.request.function
         self.basename = basename
-        self.args = current.request.raw_args and \
-            [arg for arg in current.request.raw_args.split('/') if arg] or []
+        
+        if current.request.raw_args:            
+            self.args = [arg for arg in current.request.raw_args.split('/') if arg]
+        else:
+            self.args = [arg for arg in current.request.args if args]
         filename = os.path.join(base, *self.args)
         if not os.path.exists(filename):
             raise HTTP(404, "FILE NOT FOUND")
