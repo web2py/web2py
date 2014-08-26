5d9d1d3ef6d67f30a4c3106a4bc9592f08eabeb6
gi0baro <gi0baro@d4net.org>
2014-08-26 10:51:50 +0200

    Modular DAL

---
+++ gluon/dal/_compat.py
@@ -0,0 +1,19 @@
+import sys
+import hashlib
+import os
+
+PY2 = sys.version_info[0] == 2
+
+if PY2:
+    import cPickle as pickle
+    import cStringIO as StringIO
+    hashlib_md5 = hashlib.md5
+else:
+    import pickle
+    from io import StringIO
+    hashlib_md5 = lambda s: hashlib.md5(bytes(s,'utf8'))
+
+pjoin = os.path.join
+exists = os.path.exists
+ogetattr = object.__getattribute__
+osetattr = object.__setattr__
