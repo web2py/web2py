6228de8e1022f265358c08218ce97e49eec01958
ilvalle <paolo.valleri@gmail.com>
2014-11-30 19:53:33 +0100

    Fix NULL values with SQLCustomType

--- gluon/dal/adapters/base.py
+++ gluon/dal/adapters/base.py
@@ -1323,7 +1323,7 @@ class BaseAdapter(ConnectionPool):
             value = fieldtype.encoder(obj)
             if fieldtype.type in ('string','text', 'json'):
                 return self.adapt(value)
-            return value
+            return value or 'NULL'
         if isinstance(obj, (Expression, Field)):
             return str(obj)
         if field_is_type('list:'):
