8000bda0c9ed42672612154e2a51217660095f69
gi0baro <giovanni.barillari@gmail.com>
2014-12-24 15:03:55 +0100

    Removed _default_validators impossible comparison check from DAL tests

--- gluon/tests/test_dal.py
+++ gluon/tests/test_dal.py
@@ -17,10 +17,8 @@ class TestDALSubclass(unittest.TestCase):
     def testRun(self):
         import gluon.serializers as mserializers
         from gluon import sqlhtml
-        from gluon.dal import _default_validators
         db = DAL(check_reserved=['all'])
         self.assertEqual(db.serializers, mserializers)
-        self.assertEqual(db.validators_method, _default_validators)
         self.assertEqual(db.representers['rows_render'], sqlhtml.represent)
         self.assertEqual(db.representers['rows_xml'], sqlhtml.SQLTABLE)
 
