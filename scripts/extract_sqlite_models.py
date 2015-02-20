11c90ef88589382bb4f9b9edfd3b762d6ba46201
Sven Bachmann <dev@mcbachmann.de>
2015-02-20 21:37:40 +0100

    fixed sqlite create table parse regex
    At least SQLite 3.8.6 uses the format:
    
    [item]        type,
    [item]        type,
    [item]        type,
    
    The previous variant tried to parse lines like:
    
    "item"        type,
    "item"        type,
    "item"        type,

--- scripts/extract_sqlite_models.py
+++ scripts/extract_sqlite_models.py
@@ -83,7 +83,7 @@ def sqlite(database_name):
             for line in sql_lines[1:-1]:
                 if re.search('KEY', line) or re.search('PRIMARY', line) or re.search('"ID"', line) or line.startswith(')'):
                     continue
-                hit = re.search(r'"(\S+)"\s+(\w+(\(\S+\))?),?( .*)?', line)
+                hit = re.search(r'\[(\S+)\]\s+(\w+(\(\S+\))?),?( .*)?', line)
                 if hit is not None:
                     name, d_type = hit.group(1), hit.group(2)
                     d_type = re.sub(r'(\w+)\(.*', r'\1', d_type)
