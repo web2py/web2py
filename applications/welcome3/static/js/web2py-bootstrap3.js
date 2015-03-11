71aac68c2078043e5caa1e5c250e9ad3bd33c0bf
mdipierro <massimo.dipierro@gmail.com>
2015-03-11 00:35:24 -0500

    fixed whitespaces in welcome3

--- applications/welcome3/static/js/web2py-bootstrap3.js
+++ applications/welcome3/static/js/web2py-bootstrap3.js
@@ -3,7 +3,7 @@
  * Developed by Paolo Caruccio ( paolo.caruccio66@gmail.com )
  * Released under MIT license
  * version 1 rev.201402261600
- * 
+ *
  * Supported version of bootstrap framework: 3.0.2+
 
  * The full package includes:
@@ -19,7 +19,7 @@ jQuery(function(){
         var fh_label_class = 'col-md-4',
             fh_offest_class = 'col-md-offset-4',
             fh_control_class = 'col-md-8';
-        
+
         // functions
         function menu_is_collapsed() {
             return !jQuery('.navbar-toggle').is(':hidden');
@@ -41,7 +41,7 @@ jQuery(function(){
             if (settings.parent)
                 parent = this.parent();
             else
-                parent = window;        
+                parent = window;
             if (settings.mode != "horizontally") {
                 this.css("top", Math.max
                          (0, ((jQuery(parent).height() - jQuery(this).outerHeight()) / 2) + jQuery(parent).scrollTop()) + "px");
@@ -52,10 +52,10 @@ jQuery(function(){
             }
             return this;
         };
-        
+
         // alert centering
         jQuery('.flash.alert.centered').center({'mode': "horizontally"});
-        
+
         // navs
         jQuery(".nav ul.dropdown-menu").not("#w2p-auth-bar").each(function() {
                 var toggle = jQuery(this).parent();
@@ -73,10 +73,10 @@ jQuery(function(){
         jQuery('.navbar-nav a.dropdown-toggle').click(function(e) {
                 if (menu_is_collapsed())
                     e.preventDefault();
-                else 
+                else
                     window.location=jQuery(this).attr('href');
             });
-        
+
         jQuery(".dropdown-submenu>a").click(function(event) {
                 if (menu_is_collapsed()) {
                     event.preventDefault();
@@ -99,7 +99,7 @@ jQuery(function(){
                     window.location=jQuery(this).attr('href');
                 };
             });
-        
+
         jQuery(".nav-tabs .web2py-menu-active").addClass('active');
         jQuery(".nav-tabs a").not(".dropdown-toggle").attr("data-toggle", "tab");
         // button fixes
@@ -118,9 +118,9 @@ jQuery(function(){
                     cn.removeClass('short-screen');
             }
         };
-        
+
         adjust_maxheight_of_collapsed_nav();
-        
+
         // resize and orientation change events
         jQuery(window).on('orientationchange resize', function(event) {
                 adjust_maxheight_of_collapsed_nav();
