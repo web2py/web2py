2755354e2e6d4210d7aa5e57407895a2e026f460
mdipierro <massimo.dipierro@gmail.com>
2015-03-31 17:15:19 -0500

    fixed a problem with circular imports

--- gluon/sqlhtml.py
+++ gluon/sqlhtml.py
@@ -31,7 +31,6 @@ from pydal.objects import Table, Row, Expression, Field
 from pydal.adapters.base import CALLABLETYPES
 from pydal.helpers.methods import smart_query, bar_encode
 from pydal.helpers.classes import Reference, SQLCustomType
-from gluon.dal import _default_validators
 from gluon.storage import Storage
 from gluon.utils import md5_hash
 from gluon.validators import IS_EMPTY_OR, IS_NOT_EMPTY, IS_LIST_OF, IS_DATE
@@ -1130,8 +1129,8 @@ class SQLFORM(FORM):
             extra_field.table = table
             extra_field.tablename = table._tablename
             if extra_field.requires == DEFAULT:
-                extra_field.requires = _default_validators(table._db,
-                                                           extra_field)
+                from gluon.dal import _default_validators
+                extra_field.requires = _default_validators(table._db, extra_field)
 
         for fieldname in self.fields:
             if fieldname.find('.') >= 0:
