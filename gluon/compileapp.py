4125a97ce18b5e94bc3de6000426b27a68b33004
mdipierro <massimo.dipierro@gmail.com>
2015-12-18 04:39:51 -0600

    fixed compileapp problem in appveyor test

--- gluon/compileapp.py
+++ gluon/compileapp.py
@@ -476,9 +476,9 @@ def compile_views(folder, skip_failed_views=False):
             data = parse_template(fname, path)
         except Exception, e:
             if skip_failed_views:
-                failed_views.append(file)
+                failed_views.append(fname)
             else:
-                raise Exception("%s in %s" % (e, file))
+                raise Exception("%s in %s" % (e, fname))
         else:
             filename = ('views/%s.py' % file).replace('/', '_').replace('\\', '_')
             filename = pjoin(folder, 'compiled', filename)
