4519985dc255145e5fd7d827851ea8c2f615f563
mdipierro <massimo.dipierro@gmail.com>
2016-07-29 08:53:56 -0500

    fixed indentation in webclient, issue #1396

--- gluon/contrib/webclient.py
+++ gluon/contrib/webclient.py
@@ -154,7 +154,7 @@ class WebClient(object):
         if 'set-cookie' in self.headers:
             for item in self.headers['set-cookie'].split(','):
                 key, value = item[:item.find(';')].split('=')
-            self.cookies[key.strip()] = value.strip()
+                self.cookies[key.strip()] = value.strip()
 
         # check is a new session id has been issued, symptom of broken session
         if self.session_regex is not None:
