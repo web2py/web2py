9a2d696c8f2bb27d36f5e001ab0afb194146f7fa
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2016-08-08 03:13:53 -0700

    Custom 503 pages
    Added support for custom down for maintenance pages on disabled apps,
    as suggested by Richard:
    https://groups.google.com/d/msg/web2py-developers/guV2od3TV6c/T-7ZJX6dWe
    EJ

--- gluon/main.py
+++ gluon/main.py
@@ -390,7 +390,11 @@ def wsgibase(environ, responder):
                                    % 'invalid request',
                                    web2py_error='invalid application')
                 elif not request.is_local and exists(disabled):
-                    raise HTTP(503, "<html><body><h1>Temporarily down for maintenance</h1></body></html>")
+                    if os.path.exists(os.path.join(request.folder,'static','503.html')):
+                        import urllib
+                        raise HTTP(503, urllib.urlopen(os.path.join(request.folder,'static','503.html')).read())
+                    else:
+                        raise HTTP(503, "<html><body><h1>Temporarily down for maintenance</h1></body></html>")
 
                 # ##################################################
                 # build missing folders
