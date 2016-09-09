aee0f8dbc578bd80fa3a01af09e47b0779266ea8
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2016-09-09 01:59:14 -0700

    Stripped unused strings from translations

--- applications/welcome/languages/plural-en.py
+++ applications/welcome/languages/plural-en.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 {
-# "singular form (0)": ["first plural form (1)", "second plural form (2)", ...],
-'account': ['accounts'],
-'book': ['books'],
-'is': ['are'],
-'man': ['men'],
-'miss': ['misses'],
-'person': ['people'],
-'quark': ['quarks'],
-'shop': ['shops'],
-'this': ['these'],
-'was': ['were'],
-'woman': ['women'],
+'account': "['accounts']",
+'book': "['books']",
+'is': "['are']",
+'man': "['men']",
+'miss': "['misses']",
+'person': "['people']",
+'quark': "['quarks']",
+'shop': "['shops']",
+'this': "['these']",
+'was': "['were']",
+'woman': "['women']",
 }
--- applications/welcome/languages/plural-es.py
+++ applications/welcome/languages/plural-es.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 {
-# "singular form (0)": ["first plural form (1)", "second plural form (2)", ...],
-'fila': ['filas'],
-'eliminada': ['eliminadas'],
-'actualizada': ['actualizadas'],
-'seleccionado': ['seleccionados'],
+'actualizada': "['actualizadas']",
+'eliminada': "['eliminadas']",
+'fila': "['filas']",
+'seleccionado': "['seleccionados']",
 }
