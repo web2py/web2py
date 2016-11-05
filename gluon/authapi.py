85c68e6876a320bb6f5c8ad4e377aaecfbe0ae5f
Leonel Câmara <leonelcamara@gmail.com>
2016-11-05 16:52:42 +0000

    typo

--- gluon/authapi.py
+++ gluon/authapi.py
@@ -4,7 +4,7 @@
 | Copyrighted by Massimo Di Pierro <mdipierro@cs.depaul.edu>
 | License: LGPLv3 (http://www.gnu.org/licenses/lgpl.html)
 """
-from gluo._compat import long
+from gluon._compat import long
 from gluon import current
 from gluon.storage import Messages, Settings, Storage
 from gluon.utils import web2py_uuid
