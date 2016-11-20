757d46274ee43108d5c67bd9eca03624a0aa01da
Leonel Câmara <leonelcamara@gmail.com>
2016-11-20 20:00:51 +0000

    fix bug with new login_user

--- gluon/authapi.py
+++ gluon/authapi.py
@@ -698,12 +698,13 @@ class AuthAPI(object):
                 value = user[key]
                 if callable(value) or key == self.settings.password_field:
                     delattr(user, key)
+        return user
 
     def login_user(self, user):
         """
         Logins the `user = db.auth_user(id)`
         """
-        self._update_session_user(user)
+        user = self._update_session_user(user)
         if self.settings.renew_session_onlogin:
             current.session.renew(clear_session=not self.settings.keep_session_onlogin)
         current.session.auth = Storage(user=user,
