ea5ea6a30759a2c825f23381540dc396cbc475b7
Leonel Câmara <leonelcamara@gmail.com>
2018-04-20 16:55:05 +0100

    Release the lock even if function call fails

--- gluon/contrib/redis_cache.py
+++ gluon/contrib/redis_cache.py
@@ -160,12 +160,14 @@ class RedisClient(object):
                     randomvalue = time.time()
                     al = acquire_lock(self.r_server, lock_key, randomvalue)
                     # someone may have computed it
-                    obj = self.r_server.get(newKey)
-                    if obj is None:
-                        value = self.cache_it(newKey, f, time_expire)
-                    else:
-                        value = pickle.loads(obj)
-                    release_lock(self, lock_key, al)
+                    try:
+                        obj = self.r_server.get(newKey)
+                        if obj is None:
+                            value = self.cache_it(newKey, f, time_expire)
+                        else:
+                            value = pickle.loads(obj)
+                    finally:
+                        release_lock(self, lock_key, al)
                 else:
                     # without distributed locking
                     value = self.cache_it(newKey, f, time_expire)
