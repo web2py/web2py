52672530b7d912a2df1cfc0cc66d1f29e4f9b324
mavignau@gmail.com <mavignau@gmail.com>
2018-06-19 21:13:53 -0300

    Fix proposed to crash in linux on import markdown module

--- gluon/contrib/markdown/markdown2.py
+++ gluon/contrib/markdown/markdown2.py
@@ -4,7 +4,8 @@
 # License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
 from __future__ import generators
-
+from __future__ import print_function
+cd Py   
 r"""A fast and complete Python implementation of Markdown.
 
 [from http://daringfireball.net/projects/markdown/]
@@ -35,7 +36,6 @@ This implementation of Markdown implements the full "core" syntax plus a
 number of extras (e.g., code syntax coloring, footnotes) as described on
 <https://github.com/trentm/python-markdown2/wiki/Extras>.
 """
-from __future__ import print_function
 
 cmdln_desc = """A fast and complete Python implementation of Markdown, a
 text-to-HTML conversion tool for web writers.
