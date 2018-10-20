5887e43248bfceac9de1e586c2e2241c9222ec55
Veiko Aasa <veiko.aasa@nlib.ee>
2018-10-20 13:58:41 +0300

    Fixes #1829

--- gluon/widget.py
+++ gluon/widget.py
@@ -1288,9 +1288,10 @@ end tell
         line = py2exe_getline(filename, lineno, *args, **kwargs)
         if not line:
             try:
-                f = open(filename, "r")
+                f = open(filename, "rb")
                 try:
                     for i, line in enumerate(f):
+                        line = line.decode('utf-8')
                         if lineno == i + 1:
                             break
                     else:
