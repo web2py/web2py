web2py/web2py

commit 1457e12f702d203c1c6b4eb731f7edb3057bfaf2

Don't remember password on failed login

gluon/tools.py
@@ -2637,8 +2637,9 @@ class Auth(AuthAPI):
                         # invalid login
                         session.flash = specific_error if self.settings.login_specify_error else self.messages.invalid_login
                         callback(onfail, None)
+                        request.post_vars['password'] = ""
                         redirect(
-                            self.url(args=request.args, vars=request.post_vars),
+                            self.url(args=request.args, get_vars=request.get_vars, post_vars=request.post_vars),
                             client_side=settings.client_side)
 
             else:  # use a central authentication server

commit b21cfb53f34dc8d224196c4a0e1f4b4586827a2d

Fix redirect url when login failed (invalid login)
Hello,

This is broken since 1457e12f702d203c1c6b4eb731f7edb3057bfaf2 . 'self.url' does'nt have any get_vars,post_vars, only one parameter called vars. I suggest to pass all variables instead  with request.vars  that will encompass both get and post vars... Another possibility would be to change the self.url signature but I don't see the purpose of making the distinction between post and get vars in this case.

gluon/tools.py
@@ -2640,9 +2640,7 @@ class Auth(AuthAPI):
                         callback(onfail, None)
                         if 'password' in request.post_vars:
                             del request.post_vars['password']
-                        redirect(
-                            self.url(args=request.args, get_vars=request.get_vars, post_vars=request.post_vars),
-                            client_side=settings.client_side)
+                        redirect(self.url(args=request.args, vars=request.vars),client_side=settings.client_side)
 
             else:  # use a central authentication server
                 cas = settings.login_form