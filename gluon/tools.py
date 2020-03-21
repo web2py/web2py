6c4649d992ad0cf234abdc6d5865451e2ab649e8
mdipierro <massimo.dipierro@gmail.com>
2020-03-21 12:40:23 -0700

    fixed issue #2217, bs4, thanks romicasal

--- gluon/tools.py
+++ gluon/tools.py
@@ -984,15 +984,15 @@ def addrow(form, a, b, c, style, _id, position=-1):
                                      DIV(b, SPAN(c, _class='inline-help'),
                                          _class='controls'),
                                      _class='control-group', _id=_id))
-    elif style == "bootstrap3_inline":
+    elif style in ("bootstrap3_inline", "bootstrap4_inline"):
         form[0].insert(position, DIV(LABEL(a, _class='control-label col-sm-3'),
                                      DIV(b, SPAN(c, _class='help-block'),
                                          _class='col-sm-9'),
-                                     _class='form-group', _id=_id))
-    elif style == "bootstrap3_stacked":
+                                     _class='form-group row', _id=_id))
+    elif style in ("bootstrap3_stacked", "bootstrap4_stacked"):
         form[0].insert(position, DIV(LABEL(a, _class='control-label'),
                                      b, SPAN(c, _class='help-block'),
-                                     _class='form-group', _id=_id))
+                                     _class='form-group row', _id=_id))
     else:
         form[0].insert(position, TR(TD(LABEL(a), _class='w2p_fl'),
                                     TD(b, _class='w2p_fw'),
