web2py/web2py

commit 0e788d396509f931fc81e1f2a216ecae8089d0cf

add list arg support to mail.send(reply_to)

gluon/tools.py
@@ -739,7 +739,7 @@ class Mail(object):
         if to:
             payload['To'] = encoded_or_raw(to_unicode(', '.join(to), encoding))
         if reply_to:
-            payload['Reply-To'] = encoded_or_raw(to_unicode(reply_to, encoding))
+            payload['Reply-To'] = encoded_or_raw(to_unicode(', '.join(reply_to), encoding))
         if cc:
             payload['Cc'] = encoded_or_raw(to_unicode(', '.join(cc), encoding))
             to.extend(cc)

commit dbf731d11f5023f571a224b7bb3faceac691d63f

Fix reply_to broken when only a string, broken in 0e788d396509f931fc81e1f2a216ecae8089d0cf

gluon/tools.py
@@ -454,6 +454,9 @@ class Mail(object):
                 to = [to]
         else:
             raise Exception('Target receiver address not specified')
+        if reply_to:
+            if not isinstance(reply_to, (list, tuple)):
+                reply_to = [reply_to]            
         if cc:
             if not isinstance(cc, (list, tuple)):
                 cc = [cc]