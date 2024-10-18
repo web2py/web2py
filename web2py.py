2fa64a61b75ce44bf2ed1a7f49a742937a5bf72f
Manjusri Arumugam <msri10792@gmail.com>
2024-10-18 21:38:22 +0530

    Update web2py.py

--- web2py.py
+++ web2py.py
@@ -2,57 +2,47 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-
 import os
 import sys
 from multiprocessing import freeze_support
 
+# Determine the path of the executable or script
 if hasattr(sys, 'frozen'):
-    # py2exe
     path = os.path.dirname(os.path.abspath(sys.executable))
 elif '__file__' in globals():
     path = os.path.dirname(os.path.abspath(__file__))
 else:
-    # should never happen
     path = os.getcwd()
 
-# process -f (--folder) option
+# Process -f (--folder) option
+fi = None
 if '-f' in sys.argv:
     fi = sys.argv.index('-f')
-    # maybe session2trash arg
-    if '-A' in sys.argv and  fi > sys.argv.index('-A'):
-        fi = None
 elif '--folder' in sys.argv:
     fi = sys.argv.index('--folder')
-else:
-    fi = None
-if fi and fi < len(sys.argv):
-    fi += 1
-    folder = sys.argv[fi]
+
+if fi is not None and fi < len(sys.argv) - 1:
+    folder = sys.argv[fi + 1]
     if not os.path.isdir(os.path.join(folder, 'gluon')):
-        print("%s: error: bad folder %s" % (sys.argv[0], folder), file=sys.stderr)
+        print(f"{sys.argv[0]}: error: bad folder {folder}", file=sys.stderr)
         sys.exit(1)
-    path = sys.argv[fi] = os.path.abspath(folder)
+    path = os.path.abspath(folder)
 
 os.chdir(path)
+sys.path = [path] + [p for p in sys.path if p != path]
 
-sys.path = [path] + [p for p in sys.path if not p == path]
-
-# important that this import is after the os.chdir
-
-# import gluon.import_all # NOTE: should this be uncommented for py2exe.py ?
-import gluon.widget
+# Important that this import is after changing the directory
+import gluon.widget  # Ensure that gluon is properly installed
 
 if __name__ == '__main__':
-    freeze_support()
-    # support for sub-process coverage,
-    # see https://coverage.readthedocs.io/en/coverage-4.3.4/subprocess.html
+    freeze_support()  # Required for multiprocessing on Windows
+    # Support for sub-process coverage
     if 'COVERAGE_PROCESS_START' in os.environ:
         try:
             import coverage
             coverage.process_startup()
-        except:
+        except ImportError:
             print('Coverage is not available')
-            pass
-    # start services
+    
+    # Start services
     gluon.widget.start()
