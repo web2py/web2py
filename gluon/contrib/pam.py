7dc72576880321ca59fcddc0045f617549e96907
Darren Carreras <283775510+carrerasdarren-cell@users.noreply.github.com>
2026-07-22 20:09:12 -0400

    Fix PAM account authorization check

--- gluon/contrib/pam.py
+++ gluon/contrib/pam.py
@@ -88,6 +88,10 @@ PAM_AUTHENTICATE = LIBPAM.pam_authenticate
 PAM_AUTHENTICATE.restype = c_int
 PAM_AUTHENTICATE.argtypes = [PamHandle, c_int]
 
+PAM_ACCT_MGMT = LIBPAM.pam_acct_mgmt
+PAM_ACCT_MGMT.restype = c_int
+PAM_ACCT_MGMT.argtypes = [PamHandle, c_int]
+
 
 def authenticate(username, password, service="login"):
     """Returns True if the given username and password authenticate for the
@@ -124,6 +128,8 @@ def authenticate(username, password, service="login"):
         return False
 
     retval = PAM_AUTHENTICATE(handle, 0)
+    if retval == 0:
+        retval = PAM_ACCT_MGMT(handle, 0)
     return retval == 0
 
 
